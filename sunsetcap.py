7550d6f1cf5953a261a16863b1c4b611e8249911
alberttluo <47925599+alberttluo@users.noreply.github.com>
2022-12-27 22:16:34 -0800

    Add get sunset time function

---
+++ sunsetcap.py
@@ -0,0 +1,21 @@
+import datetime
+import time
+from suntime import Sun, SunTimeException
+import schedule
+
+# Get sunset time daily
+def getSSTime():
+    latitude = 34.41
+    longitude = -118.59
+
+    sun = Sun(latitude, longitude)
+
+    today_ss = sun.get_sunset_time()
+
+    return(today_ss)
+
+schedule.every().day.at("00:00").do(getSSTime)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
